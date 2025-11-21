d049b1cdc3c9dd43cbcc0c87c760f7629b34f3fc
Enea Maroncelli <ENEA.MARONCELLI@local.ittcesena.it>
2025-11-21 08:24:26 +0100

    Aggiunte proprieta e costruttore

--- Libro.cs
+++ Libro.cs
@@ -8,5 +8,18 @@ namespace VerificaGIT
 {
     internal class Libro
     {
+        public string Autore { get; set; }
+        public string Titolo { get; set; }
+        public int AnnoDiPubblicazione { get; set; }
+        public string Editore  { get; set; }
+        public int NumeroPagine { get; set; }
+        public Libro(string author, string title, int year, string editor,int pages)
+        {
+            Autore = author;
+            Titolo = title;
+            AnnoDiPubblicazione = year;
+            Editore = editor;
+            NumeroPagine = pages;
+        }
     }
 }
