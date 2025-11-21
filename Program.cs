8cc6a98ec1498b24e9f7ffb0f3533201fbac86a4
Enea Maroncelli <ENEA.MARONCELLI@local.ittcesena.it>
2025-11-21 09:35:34 +0100

    Fixati un po di bug

--- Program.cs
+++ Program.cs
@@ -23,21 +23,21 @@
             string chiusurabiblioteca = Console.ReadLine();
             Biblioteca biblioteca  = new Biblioteca(nomebiblioteca,indirizzobiblioteca,aperturabiblioteca,chiusurabiblioteca);
             Console.WriteLine("Biblioteca registrata");
-            Console.WriteLine("Quanti libri vuole aggiungere?\n> ");
+            Console.Write("Quanti libri vuole aggiungere?\n> ");
             int numerolibri;
-            while (int.TryParse(Console.ReadLine(), out numerolibri))
+            while (!int.TryParse(Console.ReadLine(), out numerolibri))
             {
                 Console.Write("Inserisca un valore valido\n> ");
             }
             for(int i = 0; i< numerolibri; i++)
             {
-                Console.Write("Inserisca il nome del libro\n> ");
+                Console.Write("Inserisca l'autore del libro\n> ");
                 string nomelibro = Console.ReadLine();
                 Console.Write("Inserire il titolo del libro\n> ");
                 string titololibro = Console.ReadLine();
-                Console.WriteLine("Inserire solo l'anno di pubblicazione\n> ");
+                Console.Write("Inserire solo l'anno di pubblicazione\n> ");
                 int annolibro;
-                while (int.TryParse(Console.ReadLine(), out annolibro))
+                while (!int.TryParse(Console.ReadLine(), out annolibro))
                 {
                     Console.Write("Inserisca un valore valido\n> ");
                 }
@@ -45,14 +45,14 @@
                 string editorelibro  = Console.ReadLine();
                 Console.Write("Inserire quante pagine ha questo libro\n> ");
                 int paginelibro;
-                while (int.TryParse(Console.ReadLine(), out paginelibro))
+                while (!int.TryParse(Console.ReadLine(), out paginelibro))
                 {
                     Console.Write("Inserisca un valore valido\n> ");
                 }
                 Libro libro = new Libro(nomelibro,titololibro, annolibro,editorelibro,paginelibro);
                 
                 biblioteca.NuovoLibro(libro);
-                Console.WriteLine("Libro aggiunto alla biblioteca con successo, ecco alcune caratteristiche del libro: " + libro.toString() + "e quanto ci si mette a leggerlo " + libro.readingTime())
+                Console.WriteLine("Libro aggiunto alla biblioteca con successo, ecco alcune caratteristiche del libro: " + libro.toString() + " e quanto ci si mette a leggerlo " + libro.readingTime());
             }
             Console.WriteLine("Cosa vuoi fare?");
             Menu();
@@ -63,12 +63,12 @@
                 switch(scelta)
                 {
                     case "1":
-                        biblioteca.NumeroLibri();
+                        Console.WriteLine("Ci sono: " + biblioteca.NumeroLibri().ToString());
                         break;
                     case "2":
                         Console.Write("Inserisca il titolo per il quale si vuole cercare\n> ");
                         string titolo = Console.ReadLine();
-                        biblioteca.RicercaPerTitolo(titolo);
+                        Console.WriteLine(biblioteca.RicercaTitolo(titolo));
                         break;
                     case "3":
                         Console.Write("Inserire l'autore del quale si vogliono cercare i libri\n>");
@@ -77,13 +77,13 @@
                         libritrovati = biblioteca.RicercaPerAutore(autore);
                         if(libritrovati.Count() == 0)
                         {
-                            Console.WriteLine("Mi dispiace ma non abbiamo libri di questo autore in biblioteca")
+                            Console.WriteLine("Mi dispiace ma non abbiamo libri di questo autore in biblioteca");
                         } else
                         {
                             Console.WriteLine("Libri trovati:");
                             foreach(Libro libro in libritrovati)
                             {
-                                Console.WriteLine(libro.toString())
+                                Console.WriteLine(libro.toString());
                             }
                         }
                         break;
