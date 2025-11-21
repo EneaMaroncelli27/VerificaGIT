9e2d98503ae18f83ac7ba91c093ea21be16edb7b
Enea Maroncelli <ENEA.MARONCELLI@local.ittcesena.it>
2025-11-21 08:37:42 +0100

    Create proprieta e costruttore classe biblioteca

--- Biblioteca.cs
+++ Biblioteca.cs
@@ -8,5 +8,19 @@ namespace VerificaGIT
 {
     internal class Biblioteca
     {
+        public string Nome { get; set; }
+        public string Indirizzo { get; set; }
+        public string OrarioApertura { get; set; }
+        public string OrarioChiusura { get; set; }
+        public List<Libro> ListaLibro { get; set; }
+
+        public Biblioteca(string name,string address,string open,string close)
+        {
+            Nome = name;
+            Indirizzo = address;
+            OrarioApertura = open;
+            OrarioChiusura = close;
+        }
+
     }
 }
